350cbc1e9b4bebc8bc77c16851cb2597fe845b2c
Diego G. lima <diego.lima@alice.com>
2025-05-12 21:36:08 -0300

    fix payment status

--- lib/interface_adapters/gateways/clients/pedidos.ex
+++ lib/interface_adapters/gateways/clients/pedidos.ex
@@ -1,12 +1,23 @@
 defmodule FoodOrderPagamento.InterfaceAdapters.Gateways.Clients.Pedidos do
   @behaviour FoodOrderPagamento.InterfaceAdapters.Gateways.OrderGatewayBehavior
 
+  @payment_status_id %{
+    "Pagamento Realizado" => 0,
+    "Realizado" => 0,
+    "Pagamento Rejeitado" => 1,
+    "Rejeitado" => 1,
+    "Aguardando Pagamento" => 2,
+    "Aguardando" => 2
+  }
+
+  @status_default 0
+
   @impl true
   def update_payment_status(payment, payment_status) do
       client()
       |> Tesla.put("/Pedido/AtualizarStatuspagamento", %{
         numeroPedido: payment.order_id,
-        status: payment_status.status
+        status: handle_status(payment_status.status)
       })
     |> case do
       {:ok, %{status: status, body: _body}} when status >= 200 and status < 300 ->
@@ -19,6 +30,8 @@ defmodule FoodOrderPagamento.InterfaceAdapters.Gateways.Clients.Pedidos do
         {:error, error}
     end
   end
+
+  defp handle_status(status), do: Map.get(@payment_status_id, status, @status_default)
 
   defp client do
     Tesla.client([
